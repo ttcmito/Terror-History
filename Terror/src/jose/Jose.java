38e9617f0dadb8bdb6369b894edd3edf950d3e66
kuro <aniita2004@gmail.com>
2024-11-27 17:21:01 +0100

    Correción José

--- Terror/src/jose/Jose.java
+++ Terror/src/jose/Jose.java
@@ -3,21 +3,21 @@ package jose;
 public class Jose {
 
 	public static void main(String[] args) {
+		
 		System.out.println("SU HISTORIA:\n"
-				+ "José vivía en la mansion del padre de Shiro\n" 
-				+ "siempre rodeado de un silencio que él mismo había elegido\n"
-				+ "El iba ha heredar la mansion del padre de shiro , pero al nacer Shiro, su padre quería darsela \n"
-				+ "a Shiro, y esto hizo ue Jose sintiera un gran desprecio y odio hacía ella\n"
-				+ "Una noche, bajo el puente, mientras Shiro jugaba la asesino\n"
-				+ "Años mas tarde, cuando el padre de Shiro fallecio, Jose y su hijo se mudarón a la mansión heredara.\n"
-				+ "Pero desde que se mudarón han ocurrido sucesos muy extraños.");
+				+ "José vivía en la mansión del padre de Shiro siempre rodeado\n" 
+				+ "de un silencio que él mismo había elegido.\n"
+				+ "Él iba a heredar la mansión del padre de Shiro, pero al nacer Shiro, su padre quería dársela\n"
+				+ "a Shiro, y esto hizo que José sintiera un gran desprecio y odio hacía ella\n"
+				+ "Una noche, bajo el puente, mientras Shiro jugaba la asesino.\n"
+				+ "Años más tarde, cuando el padre de Shiro falleció, José y su hijo se mudaron a la mansión heredara.\n"
+				+ "Pero desde que se mudaron han ocurrido sucesos muy extraños.");
 		System.out.println();
 		
 		System.out.println("APARIENCIA FÍSICA:\n"
-				+ "jose es un hombe de 47 años gordo  con altura de 1.90 .\n"
-				+ "Es muy feo y tiene algunas marcas en su cara \n"
-				+ "El le gusta las joyrias y accesories el ponga una cadena muy gorda y colgante y unos anillos  \n"
-				+ "tiene tatuajes en el brazo izquierdo .");
+				+ "José es un hombre de 47 años gordo con altura de 1.90.\n"
+				+ "Es muy feo y tiene algunas marcas en su cara.\n"
+				+ "Tiene un gran gusto por la joyería y tiene un anillo con una pirámide.");
 		System.out.println();
 
 		System.out.println("PERSONALIDAD:\n"
@@ -25,7 +25,7 @@ public class Jose {
 				+ "Era reservado, con dificultades para expresar sus emociones, lo que lo hacía parecer frío e inaccesible.\n"
 				+ "A menudo reflexionaba en silencio, dando la impresión de estar perdido en pensamientos inquietantes\n"
 				+ "Mostraba una dualidad: podía ser amable en ocasiones, pero también tenía impulsos incontrolables y un lado perturbador\n"
-				+ " Su falta de empatía lo llevó a cometer un acto atroz que reveló el caos interior que siempre había intentado esconder.");
+				+ "Su falta de empatía lo llevó a cometer un acto atroz que reveló el caos interior que siempre había intentado esconder.");
 		
 
 	}
