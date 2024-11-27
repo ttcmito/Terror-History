2d4f0fde486f5cc50182b18dbf6ad127582bb95b
kuro <aniita2004@gmail.com>
2024-11-27 16:11:58 +0100

    Shiro

--- Terror/src/shiro/Shiro.java
+++ Terror/src/shiro/Shiro.java
@@ -3,8 +3,27 @@ package shiro;
 public class Shiro {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("SU HISTORIA:\n"
+				+ "Cuando Shiro tenía 12 años fue brutalmente asesinada en la mansión de su padre\n" 
+				+ "por un hombre misterioso con un anillo que tenía la imagen de una pirámide.\n"
+				+ "Tras la muerte de Shiro, su espíritu quedo atrapado en la mansión, y no sé ira\n"
+				+ "hasta tener una venganza y así poder descansar en paz.");
+		System.out.println();
+		
+		System.out.println("APARIENCIA FÍSICA:\n"
+				+ "Shiro es una niña de 12 años que es un fantasma, ella tiene mide 1,50cm.\n"
+				+ "Es muy pálida, su piel es tan blanca como la nieve, con un cabello muy largo\n"
+				+ "y blanco, su peinado son dos coletas altas, ella está vestida con un gran vestido blanco\n"
+				+ "manchado de sangre, sus ojos son completamente grises, demostrando su falta de alma y vida.");
+		System.out.println();
 
+		System.out.println("PERSONALIDAD:\n"
+				+ "Shiro antes de su muerte era una niña muy tímida, pero con una gran sonrisa\n"
+				+ "Ella adoraba jugar con su peluche, un conejo negro que le regalo su padre.\n"
+				+ "Tras su muerte, su personalidad fue completamente distinta, ahora solo quiere\n"
+				+ "vengarse, hacerle el máximo daño posible a su asesino, para así, poder\n"
+				+ "descansar en paz.");
 	}
 
 }
