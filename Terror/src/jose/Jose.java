43445b54f9b7c702f725e15821ad5ad06ce7f755
amro <24.damt.amromohamed@fundacionmontessori.com>
2024-11-27 17:06:31 +0100

    jose

--- Terror/src/jose/Jose.java
+++ Terror/src/jose/Jose.java
@@ -3,7 +3,30 @@ package jose;
 public class Jose {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("SU HISTORIA:\n"
+				+ "José vivía en la mansion del padre de Shiro\n" 
+				+ "siempre rodeado de un silencio que él mismo había elegido\n"
+				+ "El iba ha heredar la mansion del padre de shiro , pero al nacer Shiro, su padre quería darsela \n"
+				+ "a Shiro, y esto hizo ue Jose sintiera un gran desprecio y odio hacía ella\n"
+				+ "Una noche, bajo el puente, mientras Shiro jugaba la asesino\n"
+				+ "Años mas tarde, cuando el padre de Shiro fallecio, Jose y su hijo se mudarón a la mansión heredara.\n"
+				+ "Pero desde que se mudarón han ocurrido sucesos muy extraños.");
+		System.out.println();
+		
+		System.out.println("APARIENCIA FÍSICA:\n"
+				+ "jose es un hombe de 47 años gordo  con altura de 1.90 .\n"
+				+ "Es muy feo y tiene algunas marcas en su cara \n"
+				+ "El le gusta las joyrias y accesories el ponga una cadena muy gorda y colgante y unos anillos  \n"
+				+ "tiene tatuajes en el brazo izquierdo .");
+		System.out.println();
+
+		System.out.println("PERSONALIDAD:\n"
+				+ "José era un hombre de apariencia tranquila, pero bajo esa fachada escondía una mente atormentada y oscura\n"
+				+ "Era reservado, con dificultades para expresar sus emociones, lo que lo hacía parecer frío e inaccesible.\n"
+				+ "A menudo reflexionaba en silencio, dando la impresión de estar perdido en pensamientos inquietantes\n"
+				+ "Mostraba una dualidad: podía ser amable en ocasiones, pero también tenía impulsos incontrolables y un lado perturbador\n"
+				+ " Su falta de empatía lo llevó a cometer un acto atroz que reveló el caos interior que siempre había intentado esconder.");
+		
 
 	}
 
