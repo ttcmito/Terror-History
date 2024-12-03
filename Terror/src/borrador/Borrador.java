4a3e01de47365dd87895197955bbf5761834d99c
Mito <jaimitom1410@gmail.com>
2024-12-03 19:32:01 +0100

    un commit

--- Terror/src/borrador/Borrador.java
+++ Terror/src/borrador/Borrador.java
@@ -3,7 +3,13 @@ package borrador;
 public class Borrador {
 
 	public static void main(String[] args) {
-		// 3333 
+		
+		System.out.println("\"Hace muchos años, en Irlanda, en una mansion alejada, \\n\"\r\n"
+				+ "				+ \"ocurrio un asesinato, una niña fue brutalmente asesinada \\n\"\r\n"
+				+ "				+ \"en un puente cercano, donde el asesino se dio a la fuga,\\n\"\r\n"
+				+ "				+ \"el padre de la niña estaba gravemente enfermo y tras la noticia, y fallecio a \\n\"\r\n"
+				+ "				+ \"los pocos meses, hasta qué, Jose, el hermano del padre se mudo acompañado de su hijo \\n\"\r\n"
+				+ "				+ \" a la mansión\"");
 
 	}
 
--- Terror/src/pedro/Pedro.java
+++ Terror/src/pedro/Pedro.java
@@ -3,8 +3,10 @@ package pedro;
 public class Pedro {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		System.out.println("Pedro es un niño de 14 años, lo caracteriza el ser un chico timido \n"
+				+ "pero tambien es muy curioso por lo paranormal y muy interesado en todo ese mundo, \n"
+				+ "tendra un papel fundamental en la historia.");
 	}
 
 }
