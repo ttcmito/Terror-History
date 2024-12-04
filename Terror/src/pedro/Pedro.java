dd3484e582f956c2f8ae4cc10ef17d06bd65827b
Mito <jaimitom1410@gmail.com>
2024-12-04 16:11:33 +0100

    mi personaje

--- Terror/src/pedro/Pedro.java
+++ Terror/src/pedro/Pedro.java
@@ -7,6 +7,14 @@ public class Pedro {
 		System.out.println("Pedro es un niño de 14 años, lo caracteriza el ser un chico timido \n"
 				+ "pero tambien es muy curioso por lo paranormal y muy interesado en todo ese mundo, \n"
 				+ "tendra un papel fundamental en la historia.");
+		
+		System.out.println("APARIENCIA FISICA \n"
+				+ "Mide 1.65, impresionante a sus 14 años, es un chico de piel blanca y pelo negro \n"
+				+ "ojos cafes y fisico delgado");
+		
+		System.out.println("PERSONALIDAD \n"
+				+ "Es un chico algo timido pero a su vez curioso, lo que más le fascina son las cosas que \n"
+				+ "tengan que ver con lo paranormal.");
 	}
 
 }
