d5af68273f37a3b7d1089be859d7e1887f90b77d
spyeg <24.damt.amromohamed@fundacionmontessori.com>
2024-12-04 18:05:28 +0100

    cuarto

--- Terror/src/borrador/Borrador.java
+++ Terror/src/borrador/Borrador.java
@@ -4,17 +4,6 @@ public class Borrador {
 
 	public static void main(String[] args) {
 		
-		System.out.println("—¡Llévame a mí! ¡Déjalo a él!");
-		System.out.println();
-		System.out.println("Pero la decisión de Shiro ya estaba tomada. Un grito desgarrador resonó en la noche cuando Pedro cayó al suelo, inerte.");
-		System.out.println();
-		System.out.println("José abrazó el cuerpo de su hijo, con un dolor que parecía desgarrarle el alma. Shiro, al ver su sufrimiento, se desvaneció,\n"
-				+ "satisfecha con su venganza. La mansión quedó nuevamente en silencio.");
-		System.out.println();
-		System.out.println("Poco después, José volvió al puente. El anillo que había sellado su destino brillaba en su mano. Con una última mirada al río\n"
-				+ "que corría bajo sus pies, dejó caer el anillo y saltó, poniendo fin a su vida.");
-		System.out.println();
-		System.out.println("La vieja mansión quedó vacía una vez más, atrapada entre los ecos de un pasado oscuro y las sombras de los que jamás encontraron paz.");
-	}
+		
 
 }
