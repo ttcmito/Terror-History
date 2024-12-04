7c657a5834854a1612016937f8d91d4bc8efb782
Ana-bit666 <aniita2004@gmail.com>
2024-12-04 16:21:14 +0100

    Pedro

--- Terror/src/pedro/Pedro.java
+++ Terror/src/pedro/Pedro.java
@@ -4,17 +4,24 @@ public class Pedro {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Pedro es un niño de 14 años, lo caracteriza el ser un chico timido \n"
-				+ "pero tambien es muy curioso por lo paranormal y muy interesado en todo ese mundo, \n"
-				+ "tendra un papel fundamental en la historia.");
+		System.out.println("SU HISTORIA:\n"
+				+ "Pedro, el hijo de José, comenzó a percibir algo extraño desde el momento en que se mudaron\n"
+				+ "a la vieja mansión. Una inquietante sensación de que un oscuro misterio se escondía entre sus muros\n"
+				+ "lo acosaba día y noche. Decidido a descubrir la verdad, Pedro se embarcará en una búsqueda que podría\n"
+				+ "costarle mucho más de lo que jamás imaginó.");
+		System.out.println();
 		
-		System.out.println("APARIENCIA FISICA \n"
-				+ "Mide 1.65, impresionante a sus 14 años, es un chico de piel blanca y pelo negro \n"
-				+ "ojos cafes y fisico delgado");
+		System.out.println("APARIENCIA FÍSICA \n"
+				+ "Con 1.65 metros de estatura, impresionante para sus 14 años, Pedro es un chico de piel clara,\n"
+				+ "cabello negro azabache y ojos castaños que reflejan curiosidad. Su complexión delgada le da un aire ágil y juvenil,\n"
+				+ "propio de su edad.");
+		System.out.println();
 		
 		System.out.println("PERSONALIDAD \n"
-				+ "Es un chico algo timido pero a su vez curioso, lo que más le fascina son las cosas que \n"
-				+ "tengan que ver con lo paranormal.");
+				+ "Pedro es un chico algo tímido, pero su curiosidad insaciable lo lleva a explorar lo desconocido.\n"
+				+ "Lo que más le fascina es todo lo relacionado con lo paranormal, un mundo que despierta su imaginación\n"
+				+ "y lo impulsa a buscar respuestas. Esta inquietud lo convierte en una pieza clave dentro de la historia,\n"
+				+ "donde su interés por lo sobrenatural tendrá un papel fundamental.");
 	}
 
 }
